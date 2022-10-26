0bae36376439461a972d0b32f5900a44ca886834
Олег Маслеников <Oleg.maslenikov.2014@mail.ru>
2022-10-26 05:26:37 +0300

    collerctor-bot.version_1.1

---
+++ main.py
@@ -0,0 +1,47 @@
+from pyrogram import Client
+import time, datetime
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+api_id = os.getenv('APIID')
+api_hash = os.getenv('TOKEN')
+app = Client('my_account', api_id, api_hash)
+cash = "3 612₽"
+
+def plural_days():
+    # Делаем правильное окончание слова
+    day = ['день', 'дня', 'дней']
+    n = days()
+    if n % 10 == 1 and n % 100 != 11:
+        p = 0
+    elif 2 <= n % 10 <= 4 and (n % 100 < 10 or n % 100 >= 20):
+        p = 1
+    else:
+        p = 2
+    return day[p]
+
+def days():
+    # Высчитываем день
+    date = datetime.datetime(day=24, month=9, year=2022)
+    time_has_passed = datetime.datetime.now() - date
+    return time_has_passed.days
+
+def Send_Mess():
+    # Отправляем сообщение
+    app.send_message('me', f'Привет, я бот! Мой создатель всего-лишь хотел напомнить что ты ему должен {cash}. Уже прошло {days()} {plural_days()}. Ахуеть можно')
+ 
+def main():
+    app.start()
+    while True:
+        try:
+            Send_Mess()
+        finally:
+            time.sleep(10000)
+
+if __name__ == '__main__':
+    main()
