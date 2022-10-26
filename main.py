609801c69860b5f5625f1023c0f4569cd81147b4
Олег Маслеников <Oleg.maslenikov.2014@mail.ru>
2022-10-26 15:22:28 +0300

    collerctor-bot.version_2.1

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from pyrogram import Client
-import time, datetime
+import datetime, time
 import os
+import requests
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
-
 
 api_id = os.getenv('APIID')
 api_hash = os.getenv('TOKEN')
 app = Client('my_account', api_id, api_hash)
 cash = os.getenv('CASH')
 client = os.getenv('CLIENT')
+
+half_day = 84000
+
 
 def plural_days():
     # Делаем правильное окончание слова
@@ -26,23 +28,38 @@ def plural_days():
         p = 2
     return day[p]
 
+
 def days():
     # Высчитываем день
     date = datetime.datetime(day=24, month=9, year=2022)
     time_has_passed = datetime.datetime.now() - date
     return time_has_passed.days
 
+
+def photo_memes():
+    #забираем мемес
+    URL = 'https://meme-api.herokuapp.com/gimme'
+    response = requests.get(URL)
+    response = response.json()
+    random_cat = response.get('url')
+    return random_cat
+
+
 def Send_Mess():
     # Отправляем сообщение
-    app.send_message(client, f'Привет, я бот! Мой создатель всего-лишь хотел напомнить что ты ему должен {cash}. Уже прошло {days()} {plural_days()}. Ахуеть можно')
- 
+    app.send_message(client,
+                     f'Привет, я бот! Мой создатель всего-лишь хотел напомнить что ты ему должен {cash}. Уже прошло {days()} {plural_days()}. Ахуеть можно. Во всяком случае держи мемес на английском :)')
+    app.send_photo(client, photo_memes())
+
+
 def main():
     app.start()
     while True:
         try:
             Send_Mess()
         finally:
-            time.sleep(43000)
+            time.sleep(half_day)
+
 
 if __name__ == '__main__':
     main()
