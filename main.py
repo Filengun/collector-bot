51e732df5e211f90e71099a6a0492971a329c37a
Олег Маслеников <Oleg.maslenikov.2014@mail.ru>
2022-10-30 22:29:35 +0300

    collector-bot.version_2.1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime, time
 import os
 import requests
 
+from openpyxl import load_workbook
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,11 +11,8 @@ load_dotenv()
 api_id = os.getenv('APIID')
 api_hash = os.getenv('TOKEN')
 app = Client('my_account', api_id, api_hash)
-cash = os.getenv('CASH')
-client = os.getenv('CLIENT')
 
 half_day = 84000
-
 
 def plural_days():
     # Делаем правильное окончание слова
@@ -28,33 +26,53 @@ def plural_days():
         p = 2
     return day[p]
 
-
 def days():
     # Высчитываем день
-    date = datetime.datetime(day=24, month=9, year=2022)
-    time_has_passed = datetime.datetime.now() - date
+    time_has_passed = datetime.datetime.now() - list_date
     return time_has_passed.days
-
 
 def photo_memes():
     #забираем мемес
     URL = 'https://meme-api.herokuapp.com/gimme'
     response = requests.get(URL)
     response = response.json()
-    random_cat = response.get('url')
-    return random_cat
+    random_meme = response.get('url')
+    return random_meme
 
-
-def Send_Mess():
+def Parsing():
     # Отправляем сообщение
-    app.send_message(client, f'Привет, я бот! Мой создатель всего-лишь хотел напомнить что ты ему должен {cash}. Уже прошло {days()} {plural_days()}. Ахуеть можно. Во всяком случае держи мемес на английском :)')
-    app.send_photo(client, photo_memes())
+    wb = load_workbook('./data.xlsx')
+    sheet = wb['Лист1']
+    max = sheet.max_row
+    max_kines = 2
+    while max+1 != max_kines:   
+        list = []
+        for i in range(1, 4):
+            list.append(sheet.cell(row=max_kines, column=i).value)
+        global list_date
+        list_date = list[2]
+        Send_Mess(list)
+        max_kines += 1
+
+def Send_Mess(list):
+    days_score = days()
+    app.send_message(
+        list[0],
+        'Привет, я бот! Мой создатель всего-лишь хотел напомнить, ' \
+        f'что ты ему должен {list[1]} ₽. Уже прошло {days_score} '\
+        f'{plural_days()}. Ахуеть можно. Во всяком'\
+        'случае держи мемес на английском :)'
+    )
+    app.send_photo(
+        list[0],
+        photo_memes()
+    )
 
 def main():
     app.start()
     while True:
         try:
-            Send_Mess()
+            Parsing()
         finally:
             time.sleep(half_day)
 
